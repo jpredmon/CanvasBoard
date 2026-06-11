bb20968c250334063cd4688ae923120952e7b56d
JP Redmon <jpredmon@outlook.com>
2026-06-11 08:14:17 -0500

    feat(polish): wire EditModeBanner, deepen background to zinc-950

--- src/pages/CanvasBoardPage.tsx
+++ src/pages/CanvasBoardPage.tsx
@@ -1,4 +1,5 @@
 import { BoardHeader } from '../components/board/BoardHeader';
+import { EditModeBanner } from '../components/board/EditModeBanner';
 import { EmptyBoardState } from '../components/board/EmptyBoardState';
 import { GridCanvas } from '../components/board/GridCanvas';
 import { AddCardModal } from '../components/modals/AddCardModal';
@@ -7,10 +8,12 @@ import { selectCardCount } from '../store/cards/cardsSelectors';
 
 export default function CanvasBoardPage() {
   const cardCount = useAppSelector(selectCardCount);
+  const editMode = useAppSelector((state) => state.ui.editMode);
 
   return (
-    <div className="flex h-screen flex-col bg-zinc-900">
+    <div className="flex h-screen flex-col bg-zinc-950">
       <BoardHeader />
+      <EditModeBanner visible={editMode} />
       {cardCount === 0 ? <EmptyBoardState /> : <GridCanvas />}
       <AddCardModal />
     </div>
